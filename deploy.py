a0742c43ee484397de223beff78644e6be269b0c
MegaMind1212 <borkaryash12@gmail.com>
2023-07-20 18:12:35 +0530

    Update deploy.py with new model and prediction logic

--- deploy.py
+++ deploy.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 import joblib
+import numpy as np
 
 app = Flask(__name__)
-# Load the model
+
+# load the model
 model = joblib.load('saved_model1.joblib')
 
 @app.route('/')
@@ -12,12 +14,15 @@ def home():
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    sepal_length = float(request.form['sepal_length'])
-    sepal_width = float(request.form['sepal_width'])
-    petal_length = float(request.form['petal_length'])
-    petal_width = float(request.form['petal_width'])
-    result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
-    return render_template('index.html', **locals())
+    if request.method == 'POST':
+        sepal_length = float(request.form['sepal_length'])
+        sepal_width = float(request.form['sepal_width'])
+        petal_length = float(request.form['petal_length'])
+        petal_width = float(request.form['petal_width'])
+        input_features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+        result = model.predict(input_features)[0]
+        return render_template('index.html', result=result)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
